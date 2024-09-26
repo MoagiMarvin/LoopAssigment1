3db0d578fd0da53a06c6fac6baaa9c0a396af079
MoagiMarvin <168072297+MoagiMarvin@users.noreply.github.com>
2024-09-26 14:27:09 +0200

    Add project files.

---
+++ LoopAssigment1/BeerSong .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssigment1
+{
+       internal  class BeerSong
+    {
+        public void Song(int numbers)
+        {
+            for (int i = numbers; i > 0; i--) 
+            {
+                Console.WriteLine(i + " bottles of beer on the wall," + i + " bottles of beer.Take one down and pass it around,");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ LoopAssigment1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssigment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BeerSong song = new BeerSong();
+            song.Song(99);
+
+        }
+    }
+}
