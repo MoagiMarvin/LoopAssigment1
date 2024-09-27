ab2a5849466b640b9b840728111b4f7896e4384b
MoagiMarvin <168072297+MoagiMarvin@users.noreply.github.com>
2024-09-27 08:54:06 +0200

    newly updated

--- LoopAssigment1/BeerSong .cs
+++ LoopAssigment1/BeerSong .cs
@@ -20,7 +20,7 @@ namespace LoopAssigment1
 
                 else 
                 { 
-                    Console.WriteLine(i + " bottles of beer on the wall," + i + " bottles of beer.Take one down and pass it around.");
+                    Console.WriteLine(i + " bottle of beer on the wall," + i + " bottle of beer.Take one down and pass it around.");
                 }
             }
 
